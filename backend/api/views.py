2eda45adf56b6c4ad4acffc1a9bd3189dea7e412
Ron Almog <ron8132@gmail.com>
2024-01-04 11:09:27 -0500

    add and use serializer

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,19 +1,18 @@
 from django.forms.models import model_to_dict
-# from products.models import Product
+from django.http import JsonResponse
+from products.models import Product
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
-# from products.serializers import ProductSerializer
+from products.serializers import ProductSerializer
 
 
 # this makes it drf api view!
-# @api_view(['POST'])
+@api_view(['GET'])
 def api_home(request, *args, **kwargs):
     
-    data = request.data
-    #instance = Product.objects.all().order_by("?").first()
-    #data={}
-    #if instance:
-        # data = ProductSerializer(instance).data
+    instance = Product.objects.all().order_by("?").first()
+    data={}
+    if instance:
+         data = ProductSerializer(instance).data
 
-    data=request.data
     return Response(data)
